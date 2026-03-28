2f87df7e0ec276c374802dc00aa3021ef800036d
Paul Danao <pauldanao@Pauls-Laptop.local>
2026-03-28 14:41:22 +0800

    Supabase exclude

--- supabase/functions/send-report-notification/index.ts
+++ supabase/functions/send-report-notification/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const RESEND_API_KEY = Deno.env.get('re_MahUtqeJ_Ef3b8SNQKrbMecKzkuN3Zza4')
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL')
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')
 
